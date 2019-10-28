b3ed7a9423126382f78a34d9ce37209cee160796
Torsten Schlieder <torsten.schlieder@tomtom.com>
2019-10-28 13:44:58 +0100

    Sorting improvments

--- counter.go
+++ counter.go
@@ -14,17 +14,24 @@ type CounterImpl struct {
     wordCounts map[string]int
 }
 
-func (c *CounterImpl) Init()  {
-    c.wordCounts = make(map[string]int)
-}
-
-func (c *CounterImpl) Count(wordQueue chan string)  {
+func (c CounterImpl) Count(wordQueue chan string)  {
     for word := range wordQueue {
         c.wordCounts[word]++
     }
 }
 
-// TODO: Move sorting to extra file?
+func (c CounterImpl) Print() {
+    array := make([]WordCount, 0, len(c.wordCounts))
+    for k, v := range c.wordCounts {
+        array = append(array, WordCount{word: k, count: v})
+    }
+    sort.Sort(ByWordCount(array))
+    for _, v := range array {
+        fmt.Printf("%3d - %s\n", v.count, v.word)
+    }
+}
+
+// TODO: Move sorting (and array) to extra file?
 // Sorting helpers
 type WordCount struct {
     word string
@@ -37,7 +44,7 @@ func (a ByWordCount) Len() int {
     return len(a)
 }
 
-// TODO: Why does this work considering that "a" is passed by value?
+// TODO: Why does this work considering that "a" is passed (aka "received") by value?
 func (a ByWordCount) Swap(i, j int) {
     a[i], a[j] = a[j], a[i]
 }
@@ -46,20 +53,7 @@ func (a ByWordCount) Less(i, j int) bool {
     return a[i].count > a[j].count // TODO: Secondary sorting by value (alphabetically increasing)
 }
 
-func (c *CounterImpl) Print() {
-    array := make([]WordCount, 0, len(c.wordCounts))
-    for k, v := range c.wordCounts {
-        array = append(array, WordCount{word: k, count: v})
-    }
-    sort.Sort(ByWordCount(array))
-    for _, v := range array {
-        fmt.Printf("%3d - %s\n", v.count, v.word)
-    }
-}
-
 // Factory function
 func NewCounter() Counter {
-    var c CounterImpl
-    c.Init()
-    return &c
+    return CounterImpl{ wordCounts: make(map[string]int) }
 }
