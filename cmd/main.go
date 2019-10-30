44d8793603167e536eede566de0c2d924c7b07e7
Torsten Schlieder <torsten.schlieder@tomtom.com>
2019-10-30 17:08:22 +0100

    Adjust worker number and queue sizes

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-    numWorkers := runtime.NumCPU()
+    // Reserve one CPU for the path collector and one for the word counter (this thread)
+    numWorkers := runtime.NumCPU() - 2
+    if numWorkers < 1 { numWorkers = 1 }
 
-    // TODO: Change size of all queues for experimenting
-    pathQueue := make(chan string)
-    wordQueue := make(chan string)
+    pathQueue := make(chan string, 100)
+    wordQueue := make(chan string, 1000)
 
     workerPool := internal.NewWorkerPool(numWorkers, pathQueue, wordQueue)
     go workerPool.Work()
